11913ceea91715a1e6032a78c736f4a9ca097532
NatanimA <Natanimabesha@gmail.com>
2023-04-14 02:12:01 +0300

    Gallery insertions

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,44 +1,121 @@
-import React, { useRef, useState } from 'react'
+import React, { useState,useEffect } from 'react'
+import {getStorage, ref, uploadBytes,listAll,getDownloadURL, list } from 'firebase/storage';
+import { v4 as uuidv4} from "uuid"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
+
+import CameraMode from './CameraMode'
+import dataURItoBlob from '../../constants/util-func'
 import Profile from '../util/Profile'
 import "./Gallery.scss"
+import { storage } from '../../constants/firebase-config';
+import { allEmojis } from '../../constants/constants';
+import { set } from 'firebase/database';
 
 const Gallery = () => {
+  const [photoMode,setPhotoMode] = useState(false)
+  const [picture,setPhoto] = useState(null)
+  const [photoList,setPhotoList] = useState([])
+  const [message,setMessage] = useState("")
+  const notify =() => toast(message)
+
+  const listRef = ref(storage, 'images/');
+
+  const fetchImageUrls = async () => {
+      const imageRefs = await listAll(listRef);
+      const urls = await Promise.all(
+        imageRefs.items.map(async (itemRef) => {
+          const url = await getDownloadURL(itemRef);
+          return url;
+        })
+      );
+      setPhotoList(urls);
+    };
+
+  useEffect(() => {
+
+    fetchImageUrls();
+  },[])
 
 
+
+  const handlePhotoMode = () => {
+    setPhotoMode(true)
+  }
+
+  const handleNoPhotoMode = () => {
+    setPhotoMode(false)
+  }
+
+  const handlePhotoPost =  async () => {
+    const image = dataURItoBlob(picture)
+
+    try{
+        const storageRef = ref(storage,'images/' + uuidv4())
+
+        uploadBytes(storageRef, image).then((snapshot) => {
+            fetchImageUrls()
+            setMessage("Photo is live." + allEmojis[Math.floor(Math.random() * allEmojis.length)])
+        });
+        notify()
+        setPhotoMode(false)
+    }catch(err){
+        setMessage(err)
+        notify()
+    }
+
+  }
+
+  const handleFileSelect = e => {
+    setPhoto(e.target.files[0])
+  }
+
+  const handleFilePost = async () => {
+    try{
+        const storageRef = ref(storage,'images/' + uuidv4())
+        await uploadBytes(storageRef, picture);
+        fetchImageUrls()
+        setMessage("Photo is live." + allEmojis[Math.floor(Math.random() * allEmojis.length)])
+        setPhoto(null)
+    }catch(err){
+      setMessage(err)
+    }
+
+  }
 
   return (
     <>
-      <Profile />
-      <div className="gallery-container">
-          <div className="gallery-item" data-index="1">
-              <img src="https://images.pexels.com/photos/39811/pexels-photo-39811.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb" />
-          </div>
-          <div className="gallery-item" data-index="2">
-              <img src="https://images.pexels.com/photos/5969/wood-nature-forest-bridge.jpg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb" />
-          </div>
-          <div className="gallery-item" data-index="3">
-              <img src="https://images.pexels.com/photos/531321/pexels-photo-531321.jpeg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb" />
-          </div>
-          <div className="gallery-item" data-index="4">
-              <img src="https://images.pexels.com/photos/373920/pexels-photo-373920.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" />
-          </div>
-          <div className="gallery-item" data-index="5">
-              <img src="https://images.pexels.com/photos/164241/pexels-photo-164241.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" />
-          </div>
-          <div className="gallery-item" data-index="6">
-              <img src="https://images.pexels.com/photos/417054/pexels-photo-417054.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
-          </div>
-          <div className="gallery-item" data-index="7">
-              <img src="https://images.pexels.com/photos/414171/pexels-photo-414171.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
-          </div>
-          <div className="gallery-item" data-index="8">
-              <img src="https://images.pexels.com/photos/38326/pexels-photo-38326.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
-          </div>
-          <div className="gallery-item" data-index="9">
-              <img src="https://images.pexels.com/photos/266691/pexels-photo-266691.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
-          </div>
+      <div className='cameramode-container'>
+            <CameraMode photoMode={photoMode} handleNoPhotoMode={handleNoPhotoMode} handlePhotoMode={handlePhotoMode} />
+      </div>
+      { photoMode ?
+        (<div className='cameramode-container'>
+            <Profile handlePhotoPost={handlePhotoPost} setPhoto={setPhoto} />
+        </div>)  :
+      (
+        <div className='gallery-container gallery-input-container'>
+            <div className='gallery-item'>
+                <input onChange={(e) =>{
+                  handleFileSelect(e)
+                  }} type='file'  accept='image/*' />
+            </div>
+            {picture ? (
+              <button onClick={handleFilePost} className='btn-post'>
+                Post
+              </button>) : null}
+
         </div>
+      )
+      }
+      <div className="gallery-container">
+          { photoList && photoList.length > 0 && photoList.map( (photo,index) => (
+              <div className="gallery-item" data-index={index} key={index}>
+                <img src={photo} />
+              </div>
+          ))}
+        </div>
+        <ToastContainer />
     </>
   )
 }
