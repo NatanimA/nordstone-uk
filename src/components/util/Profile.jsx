56f918f1c7465ee7a29a2faf68d9e10ed2ddbd2e
NatanimA <Natanimabesha@gmail.com>
2023-04-13 16:22:44 +0300

    Profile components insertions

---
+++ src/components/util/Profile.jsx
@@ -0,0 +1,56 @@
+import React,{useState,useRef,useCallback} from 'react'
+import Webcam from 'react-webcam'
+
+import { videoConstraints } from '../../constants/constants'
+
+const Profile = () => {
+  const [picture, setPicture] = useState('')
+  const webcamRef = React.useRef(null)
+  const capture = React.useCallback(() => {
+    const pictureSrc = webcamRef.current.getScreenshot()
+    setPicture(pictureSrc)
+  })
+  return (
+    <div>
+      <div>
+        {picture == '' ? (
+          <Webcam
+            audio={false}
+            height={400}
+            ref={webcamRef}
+            width={400}
+            screenshotFormat="image/jpeg"
+            videoConstraints={videoConstraints}
+          />
+        ) : (
+          <img src={picture} />
+        )}
+      </div>
+      <div>
+        {picture != '' ? (
+          <button
+            onClick={(e) => {
+              e.preventDefault()
+              setPicture()
+            }}
+            className="btn btn-primary"
+          >
+            Retake
+          </button>
+        ) : (
+          <button
+            onClick={(e) => {
+              e.preventDefault()
+              capture()
+            }}
+            className="btn btn-danger"
+          >
+            Capture
+          </button>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Profile
